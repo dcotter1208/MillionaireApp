ddbf17cf9661429f4c95fa87efd9d07f4fd1fb1b
Craig Carlson <cmcarlson94@gmail.com>
2015-10-21 13:39:19 -0400

    Update questions to 10 per round and Add random number functionality

--- QAndA.swift
+++ QAndA.swift
@@ -8,49 +8,64 @@
 
 import Foundation
 
+
+var randomNumberStorage = [Int]()
+
 struct Question {
     var question: String
     var possibleAnswers = [String]()
     var correctAnswer: String
-    
 }
-
 
 struct Round {
     var name = String()
     var questions = [Question]()
     
-    func randomQuestion() -> Question {
+    func randomNumber() -> Int {
         let unsignedArrayCount = UInt32(questions.count)
         let unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
         let randomNumber = Int(unsignedRandomNumber)
-        
-        return questions[randomNumber]
+        return randomNumber
     }
     
-    func chooseAnswer(selectedRow: String) {
-        if selectedRow == randomQuestion().correctAnswer {
-            print("You chose the right answer")
-        } else {
-            print("You're wrong.")
+    func storeRandomNumber(numberToStore: Int) {
+        if randomNumberStorage.contains(numberToStore) == false {
+            randomNumberStorage.append(numberToStore)
         }
     }
     
+    func setUpQuestionsForRound() {
+        while randomNumberStorage.count < 5 {
+            storeRandomNumber(randomNumber())
+        }
+    }
+    
+    func prepareNextRound() {
+        randomNumberStorage.removeAll()
+        setUpQuestionsForRound()
+    }
+    
+    func chooseAnswer(selectedRow: Int, numberFromStorageArray: Int) {
+        print(questions[randomNumberStorage[numberFromStorageArray]].question)
+        let userAnswer = questions[randomNumberStorage[numberFromStorageArray]].possibleAnswers[selectedRow]
+        print("I pick \(userAnswer).")
+        if userAnswer == questions[randomNumberStorage[numberFromStorageArray]].correctAnswer {
+            print("You chose the correct answer: \(userAnswer)")
+        } else {
+            print("You're wrong.  The answer is \(questions[randomNumberStorage[numberFromStorageArray]].correctAnswer)")
+        }
+    }
 }
 
 struct GameRound {
     var rounds = [Round]()
 }
-
-
 //Mark: Instances of the structs...this is where our game questions are.
 
 
-var gameRounds = GameRound(rounds: [roundOne, roundTwo, roundThree])
-
 //Round One Questions
 var roundOneQuestionOne = Question(question: "Who played Beetlejuice in the 1988 film “Beetlejuice”?", possibleAnswers: ["Michael Keaton", "Alec Baldwin", "Johnny Depp", "Jeffery Jones"], correctAnswer: "Michael Keaton")
-var roundOneQuestionTwo = Question(question: "The Civil War started in what year?", possibleAnswers: ["1865", "1861", "1859", "1869"], correctAnswer: "1961")
+var roundOneQuestionTwo = Question(question: "The Civil War started in what year?", possibleAnswers: ["1865", "1861", "1859", "1869"], correctAnswer: "1861")
 var roundOneQuestionThree = Question(question: "Who was the 18th president of the United States of America?", possibleAnswers: ["Ulysses S. Grant", "Andrew Johnson", "Grover Cleveland", "William McKinley"], correctAnswer: "Ulysses S. Grant")
 var roundOneQuestionFour = Question(question: "Who voiced the character of Buzz Lightyear in the Toy Story films?", possibleAnswers: ["Tom Hanks", "John Ratzenberger", "Tim Allen", "Don Rickles"], correctAnswer: "Tim Allen")
 var roundOneQuestionFive = Question(question: "What is the approximate elevation of the city of Denver, CO?", possibleAnswers: ["6,340'", "14,780'", "3,920'", "5,280'"], correctAnswer: "5,280'")
@@ -58,17 +73,19 @@ var roundOneQuestionSix = Question(question: "What was the date of the D-Day inv
 var roundOneQuestionSeven = Question(question: "Which country was not part of the Axis Forces in WWII?", possibleAnswers: ["Germany", "Italy", "China", "Japan"], correctAnswer: "China")
 var roundOneQuestionEight = Question(question: "What was the name of the theater where John Wilkes Booth shot Abraham Lincoln?", possibleAnswers: ["Sydney Opera House", "The Globe", "Carnegie Hall", "Ford Theater"], correctAnswer: "Ford Theater")
 var roundOneQuestionNine = Question(question: "Which city does not have an NFL football team?", possibleAnswers: ["Detroit", "Las Vegas", "Houston", "Kansas City"], correctAnswer: "Las Vegas")
+var roundOneQuestionTen = Question(question: "What is special about the sentence “The Quick Brown Fox Jumps Over The Lazy Dog?”", possibleAnswers: ["It was a secret military operation during WWII", "It has all letters of the alphabet", "All capitalized letters spell out a famous city in the United States", "Famous line from Robert Frost poem"], correctAnswer: "It has all letters of the alphabet")
 
 //Round Two Questions
 var roundTwoQuestionOne = Question(question: "This tech mogul founded Broadcast.com and sold it to Yahoo in 1999", possibleAnswers: ["Mark Cuban", "Peter Thiel", "John McAfee", "Max Levchin"], correctAnswer: "Mark Cuban")
 var roundTwoQuestionTwo = Question(question: "In which Indian Jones movies does Indiana Jones go on a quest for the holy grail?", possibleAnswers: ["Raiders of the Lost Ark", "The Last Crusade", "Temple of Doom", "Kingdom of the Crystal Skull"], correctAnswer: "The Last Crusade")
-var roundTwoQuestionThree = Question(question: "The 2007 finance book “Grande Expectations” Chronicles what U.S. company’s stock prices?", possibleAnswers: ["Tim Horton's", "Starbucks", "Biggby Coffee", "Dunkin' Donuts"], correctAnswer: "Starbucks")
-var roundTwoQuestionFour = Question(question: "Which ficticous person was not a character in the movie 'A Few Good Men'?", possibleAnswers: ["Capt. Jack Ross", "Lance Cpl. Harold W. Dawson", "Col. Nathan R. Jessup", "Seargeant Michael Horvath"], correctAnswer: "Sergeant Michael Horvath")
+var roundTwoQuestionThree = Question(question: "The 2007 finance book “Grande Expectations” chronicles what U.S. company’s stock prices?", possibleAnswers: ["Tim Horton's", "Starbucks", "Biggby Coffee", "Dunkin' Donuts"], correctAnswer: "Starbucks")
+var roundTwoQuestionFour = Question(question: "Which ficticous person was not a character in the movie “A Few Good Men”?", possibleAnswers: ["Capt. Jack Ross", "Lance Cpl. Harold W. Dawson", "Col. Nathan R. Jessup", "Seargeant Michael Horvath"], correctAnswer: "Sergeant Michael Horvath")
 var roundTwoQuestionFive = Question(question: "Inside of a star, what element is formed after the fusion of two hydrogen molecules?", possibleAnswers: ["Helium", "Carbon", "Oxygen", "Nitrogen"], correctAnswer: "Helium")
 var roundTwoQuestionSix = Question(question: "Who attempted the assassination of President Ronald Reagan in 1981?", possibleAnswers: ["John Wilkes Booth", "Mark David Chapman", "James Earl Ray", "John Hinkley, Jr."], correctAnswer: "John Hinkley, Jr.")
 var roundTwoQuestionSeven = Question(question: "Which famous virologist discovered the first vaccine for polio in the 20th century?", possibleAnswers: ["Louie Pasteur", "Madame Curie", "Jonas Salk", "Philip Roth"], correctAnswer: "Jonas Salk")
 var roundTwoQuestionEight = Question(question: "What is the name of the geographical feature on the southern point of Chile in South America where the Pacific and Atlantic Oceans meet?", possibleAnswers: ["Strait of Gibraltar", "The Bering Sea", "The Bay of Pigs", "Cape Horn"], correctAnswer: "Cape Horn")
 var roundTwoQuestionNine = Question(question: "Which famous author coined ther term 'The Lost Generation' to describe the generation that came of age during the First World War", possibleAnswers: ["Ernest Hemingway", "Suzanne Collins", "Charles Dickens", "Ernest Borgnine"], correctAnswer: "Ernest Hemingway")
+var rountTwoQuestionTen = Question(question: "Bob Dylan real name is:", possibleAnswers: ["Robert Zimmerman", "Robin Zachary", "Richard Roberts", "James Johnson"], correctAnswer: "Robert Zimmerman")
 
 //Round Three Questions
 var roundThreeQuestionOne = Question(question: "In a memorable scene from “Beverly Hills Cop,” what does Detective Axel Foley use to disable a car?", possibleAnswers: ["Potatoes", "Bananas", "Shoe Laces", "Towels"], correctAnswer: "Bananas")
@@ -79,11 +96,13 @@ var roundThreeQuestionFive = Question(question: "Which particle theory did the L
 var roundThreeQuestionSix = Question(question: "How many men signed the Declaration of Independence in 1776?", possibleAnswers: ["47", "51", "56", "62"], correctAnswer: "56")
 var roundThreeQuestionSeven = Question(question: "The Emmy Award-winning mini-series Band of Brothers was based on the book of the same name written by which famous author?", possibleAnswers: ["David McCullough", "Stephen Ambrose", "David Foster Wallace", "George Plimpton"], correctAnswer: "Stephen Ambrose")
 var roundThreeQuestionEight = Question(question: "Which Academy Award-winning actor had one of his first major roles portraying the reclusive character Boo Radley in the film 'To Kill a Mockingbird", possibleAnswers: ["Robert Duvall", "Marlon Brando", "Al Pacino", "Martin Sheen"], correctAnswer: "Robert Duvall")
+var roundThreeQuestionNine = Question(question: "What was the name of the NBA basketball player who was taken directly before Michael Jordan in the 1984 NBA Draft?", possibleAnswers: ["Hakeem Olajuwon", "Sam Perkins", "Alvin Robertson", "Sam Bowie"], correctAnswer: "Sam Bowie")
+var roundThreeQuestionTen = Question(question: "Pteronophobia is the fear of:", possibleAnswers: ["Being tickled by feathers",
+    "Being attacked by dinosaurs", "Being assaulted by police officers", "Being touched by other humans"], correctAnswer: "Being tickled by feathers")
 
 
-
-var roundOne = Round(name: "Round One", questions: [roundOneQuestionOne, roundOneQuestionTwo, roundOneQuestionThree, roundOneQuestionFour, roundOneQuestionFive, roundOneQuestionSix, roundOneQuestionSeven, roundOneQuestionEight, roundOneQuestionNine])
-var roundTwo = Round(name: "Round Two", questions: [roundTwoQuestionOne, roundTwoQuestionTwo, roundTwoQuestionThree, roundTwoQuestionFour, roundTwoQuestionFive, roundTwoQuestionSix, roundTwoQuestionSeven, roundTwoQuestionEight, roundTwoQuestionNine])
-var roundThree = Round(name: "Round Three", questions: [roundThreeQuestionOne, roundThreeQuestionTwo, roundThreeQuestionThree, roundThreeQuestionFour, roundThreeQuestionFive, roundThreeQuestionSix, roundThreeQuestionSeven, roundThreeQuestionEight])
+var roundOne = Round(name: "Round One", questions: [roundOneQuestionOne, roundOneQuestionTwo, roundOneQuestionThree, roundOneQuestionFour, roundOneQuestionFive, roundOneQuestionSix, roundOneQuestionSeven, roundOneQuestionEight, roundOneQuestionNine, roundOneQuestionTen])
+var roundTwo = Round(name: "Round Two", questions: [roundTwoQuestionOne, roundTwoQuestionTwo, roundTwoQuestionThree, roundTwoQuestionFour, roundTwoQuestionFive, roundTwoQuestionSix, roundTwoQuestionSeven, roundTwoQuestionEight, roundTwoQuestionNine, rountTwoQuestionTen])
+var roundThree = Round(name: "Round Three", questions: [roundThreeQuestionOne, roundThreeQuestionTwo, roundThreeQuestionThree, roundThreeQuestionFour, roundThreeQuestionFive, roundThreeQuestionSix, roundThreeQuestionSeven, roundThreeQuestionEight, roundThreeQuestionNine, roundThreeQuestionTen])
 
 
